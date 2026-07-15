896eebcc1c445a3abc72f2926bcb249d2c1620b7
boatrocl <sorawittamanunt@gmail.com>
2026-07-15 14:21:09 +0700

    full work for send

---
+++ myproject673380295_9/src/main/java/com/example/myproject673380295_9/Myproject6733802959Application.java
@@ -0,0 +1,13 @@
+package com.example.myproject673380295_9;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Myproject6733802959Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Myproject6733802959Application.class, args);
+	}
+
+}
---
+++ myproject673380295_9/src/test/java/com/example/myproject673380295_9/Myproject6733802959ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.myproject673380295_9;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Myproject6733802959ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
